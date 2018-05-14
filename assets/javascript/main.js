5f230aeac0a5b28f998afb44a0c87d0b0dd2a7e6
ardeay <apuzzo@gmail.com>
2018-05-14 15:14:33 -0700

    dyanmic content complete

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,20 +2,21 @@
 var $testInput = $("#testInput")
 var examplesURL = "https://raw.githubusercontent.com/zesty-io/schema-reader-ui/master/assets/examples/examples.json"
 
+
+// build the dynamic content for examples
 $.get(examplesURL, function(data){
 	var i = 0
 	$.each(JSON.parse(data).tabs,function(key,value){
 		var $name = value.name
 		var active = i == 0 ? `is-active` : ``
-		var tab = `<a class="${active}" data-tab="${$name}">${$name}</a>`
-		var panel = `<div class="panel-content ${active}"  id="${$name}">`
+		var tab = `<a class="exampleTab ${active}" data-tab="${$name}">${$name}</a>`
+		var panel = `<div class="panel-content example-content ${active}"  id="${$name}">`
 		$('#exampleTabs').append(tab)
 		$('#examplePanel').append(panel)
 		i++
 
-
 		$.get(value.url,function(data2){
-			console.log(data2)
+
 			$.each(JSON.parse(data2).examples,function(key,value){
 				var html = `<a class="panel-block" href="${value.url}">
 					<span class="panel-icon">
@@ -32,12 +33,25 @@ $.get(examplesURL, function(data){
 
 })
 
-$(document).on('click',".panel-block",function(e){
+// example ui controllers
+// nav
+$(document).on('click',"a.exampleTab",function(e){
 
 	e.preventDefault()
+	var $this = $(this)
+	var name = $this.attr('data-tab')
+	$('a.exampleTab').removeClass('is-active')
+	$('.panel-content').removeClass('is-active')
+	$(`#${name}`).addClass('is-active')
+	$this.addClass('is-active')
+})
+// exmaple
+$(document).on('click',".panel-block",function(e){
+	e.preventDefault()
+	var $this = $(this)
 	$('.panel-block').removeClass('is-active')
 	$(this).addClass('is-active')
-	$.get($(this).attr('href')).done(function(data){
+	$.get($this.attr('href')).done(function(data){
 		$testInput.val('')
 		$testInput.val(data)
 		parseYAML(data)
