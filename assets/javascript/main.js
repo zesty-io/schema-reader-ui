0242791db4b2b1a89884ab14cfa6bede72f94158
ardeay <apuzzo@gmail.com>
2018-05-14 14:52:54 -0700

    hooking up dynamic content

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,20 +1,33 @@
 
 var $testInput = $("#testInput")
-var starterExamplesURL = "https://raw.githubusercontent.com/zesty-io/schema-reader-ui/master/assets/examples/starters.json"
+var examplesURL = "https://raw.githubusercontent.com/zesty-io/schema-reader-ui/master/assets/examples/examples.json"
 
-$.get(starterExamplesURL, function(data){
-	var json = JSON.parse(data)
-	$.each(json.examples,function(key,value){
-		console.log(value)
+$.get(examplesURL, function(data){
+	var i = 0
+	$.each(JSON.parse(data).tabs,function(key,value){
+		var $name = value.name
+		var active = i == 0 ? `is-active` : ``
+		var tab = `<a class="${active}" data-tab="${$name}">${$name}</a>`
+		var panel = `<div class="panel-content ${active}"  id="${$name}">`
+		$('#exampleTabs').append(tab)
+		$('#examplePanel').append(panel)
+		i++
 
-		var html = `<a class="panel-block" href="${value.url}">
-			<span class="panel-icon">
-				<i class="fas fa-book" aria-hidden="true"></i>
-			</span>
-			${value.name}
-		</a>`
 
-		$("#starters").append(html)
+		$.get(value.url,function(data2){
+			console.log(data2)
+			$.each(JSON.parse(data2).examples,function(key,value){
+				var html = `<a class="panel-block" href="${value.url}">
+					<span class="panel-icon">
+						<i class="fas fa-book" aria-hidden="true"></i>
+					</span>
+					${value.name}
+				</a>`
+
+				$(`#${$name}`).append(html)
+			})
+		})
+
 	})
 
 })
