5ad2863067786e5cf9ca4c41beff7af63f808e0d
ardeay <apuzzo@gmail.com>
2018-05-14 11:59:35 -0700

    full test working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,23 @@
-alert('hello')
+var parseEndpoint = "https://us-central1-zesty-dev.cloudfunctions.net/schemaReader"
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": parseEndpoint,
+  "method": "POST",
+  "headers": {
+    "Content-Type": "text/plain",
+    "Cache-Control": "no-cache",
+  }
+}
+
+$("#testInput").on('change',function(){
+	console.log('running')
+	var $this = $(this)
+	settings.data = $this.val()
+
+	$.ajax(settings).done(function(data){
+		console.log(data)
+		$( "#testOutput" ).html( JSON.stringify(data) );
+	})
+})
